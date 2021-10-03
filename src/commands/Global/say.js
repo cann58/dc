ae48d9bf12447619a347b20cdd76667baa87dcf5
Glitch (occipital-chatter-clove) <none>
2021-10-03 11:56:19 +0000

    🏫📇 Checkpoint ./src/commands/Global/say.js:37001606/1170

--- src/commands/Global/say.js
+++ src/commands/Global/say.js
@@ -7,17 +7,17 @@ module.exports = {
         var TotalMember = message.guild.memberCount
         var Online = message.guild.members.cache.filter(off => off.presence.status !== 'offline').size;
         var Taglı = message.guild.members.cache.filter(u => u.user.username.includes(config.registration.GuilDTag)).size;
+        var Taglı2 = message.guild.members.cache.filter(u => u.user.username.includes(config.registration.GuilDTag2)).size;
         var Etiket = message.guild.members.cache.filter(u => u.user.discriminator.includes(config.registration.GuildDiscrim)).size;
+        var ToplamTag = Etiket + Taglı + Taglı2
         var Voice = message.guild.members.cache.filter(s => s.voice.channel).size;
         var Boost = message.guild.premiumSubscriptionCount;
 
-        message.channel.send(new MessageEmbed().setFooter("Developed by Matthe").setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setDescription(`
-    \`•\` Sunucumuzda toplam **${TotalMember}** kullanıcı bulunmaktadır.
-    \`•\` Sunucumuzda toplam **${Online}** aktif kullanıcı bulunmaktadır.
-    \`•\` Seste **${Voice}** kullanıcı bulunmaktadır.
-    
-    \`•\` Toplam **${Taglı}** \`${config.registration.GuilDTag}\` kişi tagımızda bulunuyor.
-    \`•\` Toplam **${Etiket}** \`#${config.registration.GuildDiscrim}\` kişi etiket tagımızda bulunuyor.
+        message.channel.send(new MessageEmbed().setFooter("Developed by Matthe", message.guild.iconURL).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setDescription(`
+    \`•\` Sunucumuzda toplam **${TotalMember}** kullanıcı bulunuyor.
+    \`•\` Sunucumuzda toplam **${Online}** aktif kullanıcı bulunuyor.
+    \`•\` Seste **${Voice}** kullanıcı bulunuyor.
+    \`•\` Toplam **${ToplamTag}** kullanıcı tagımızda bulunuyor.
     `))
       
     }
