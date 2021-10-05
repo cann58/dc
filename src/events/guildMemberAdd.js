6ab70bedc4af329ecf88fb08843cd74663478373
Glitch (occipital-chatter-clove) <none>
2021-10-05 12:24:15 +0000

    🏀🚐 Checkpoint ./src/events/guildMemberAdd.js:37001606/4

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -57,7 +57,7 @@ module.exports = async member => {
         member.roles.add(config.registration.unregistered);
         member.roles.add(config.registration.unregistered);
         member.guild.channels.cache.get(config.channels.welcomechannel).send(`:tada: **${config.Guild.GuilDName}** sunucumuza hoş geldin ${member}!
-\`•\` Hesabın **${kuruluş}** tarihinde (${zaman}) önce oluşturulmuş.
+\`•\` Hesabın ${kuruluş} tarihinde (${zaman}) önce oluşturulmuş.
 
 \`•\` Eğer kayıt olmak istersen " V. Confirmed " odalarına geçmelisin, <@&${config.registration.staff}> rolündeki yetkililerimiz seninle ilgilenecektir.
 
